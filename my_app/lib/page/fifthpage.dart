df24cbc749c729d13be66786052acc76e0b75552
To1uen <theloneranger2320@gmail.com>
2022-10-13 13:57:28 +0700

    add my_app

---
+++ my_app/lib/page/fifthpage.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class FifthPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    appBar: AppBar(
+        title: Text('iGoss')),
+    body: Center(
+      child: Text('chưa có nội dung', style: TextStyle(fontSize: 60)
+      ),
+    ),
+  );
+}
