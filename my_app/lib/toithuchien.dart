b8f8b65561a163aa9c9da71c1791144be8cedec0
To1uen <theloneranger2320@gmail.com>
2022-10-17 11:32:04 +0700

    add my_app

--- my_app/lib/toithuchien.dart
+++ my_app/lib/toithuchien.dart
@@ -1,17 +1,44 @@
 import 'package:flutter/material.dart';
 
-class toithuchien extends StatelessWidget {
+
+class toithuchien extends StatefulWidget {
   const toithuchien({Key? key}) : super(key: key);
+
+  @override
+  State<toithuchien> createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<toithuchien> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Tôi thực hiện'),
-        backgroundColor: Colors.green[900],
-      ),
+        appBar: AppBar(
+          backgroundColor: Colors.green[900],
+          title: const Text('Tôi thực hiện'),
+          elevation: 0.0,
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.grey,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                    borderSide: BorderSide.none,),
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+            ],
+          ),
+        )
     );
-
   }
-
-
 }
+
